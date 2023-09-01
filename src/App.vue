e84a76c6e28a794fa9babf9ad383803b7a64a4f2
superwhys <269085434@qq.com>
2023-09-01 15:51:03 +0800

    project initialize

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,51 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <home-page></home-page>
   </div>
 </template>
 
 <style>
+:root {
+  --black: rgb(30, 30, 30);
+  --white: rgb(187, 187, 187);
+  --card-shadow: 0px 2px 4px 0px rgb(0, 0, 0, 0.05);
+  --full-height: 100%;
+  --window-width: 90vw;
+  --window-height: 90vh;
+  --sidebar-width: 271px;
+  --window-content-width: 100%;
+  --primary: rgb(29, 147, 171);
+}
+html {
+  height: var(--full-height);
+}
+body {
+  background-color: black;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  user-select: none;
+  touch-action: pan-x pan-y;
+  overflow: hidden;
+}
 #app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100vw;
+  overflow: hidden;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
+<script setup>
+import HomePage from "@/views/homePage.vue";
+</script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "element-ui/lib/theme-chalk/display.css";
+
+import { Aside, Container, Main } from "element-ui";
+Vue.use(Aside);
+Vue.use(Container);
+Vue.use(Main);
 
 Vue.config.productionTip = false;
 
